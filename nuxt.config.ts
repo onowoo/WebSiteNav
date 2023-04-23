c5eacbba315d027ee3d27809e2f3c1a2671e72de
onowoo <6559170@qq.com>
2023-04-23 08:43:28 +0800

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'A Better Nuxt 3 Starter',
+      title: 'A Nuxt 3 WebSitenNav',
       link: [{ rel: 'icon', type: 'image/*', href: '/favicon.svg' }]
     }
   },
